e3b64edc7f36686734620b7693e96871afafed5a
tgray017 <tgray017@gmail.com>
2019-02-26 14:41:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,7 @@ class Application
       item = @@items.find {|i| i.name == item_name}
       if item.nil?
         resp.write "Item not found"
-        resp.status = 404
+        resp.status = 400
       else
         resp.write "#{item.price}\n"
       end
