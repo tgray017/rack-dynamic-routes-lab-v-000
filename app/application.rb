39bc935fb82726842782d48c3bb3ff44ddc79f0c
tgray017 <tgray017@gmail.com>
2019-02-26 14:28:21 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Application
+  
+  @@items = Item.all
+  
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+  
+    if req.path.match(/item/)
+      binding.pry
+      @@items.each do |item|
+        resp.write "#{item.price}\n"
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    
+  resp.finish
+  end
+  
+end
