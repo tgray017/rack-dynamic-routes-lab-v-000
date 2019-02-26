7e6a39708de47a012fa7285bc125165e73045cbb
tgray017 <tgray017@gmail.com>
2019-02-26 14:37:50 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,10 +9,11 @@ class Application
     resp = Rack::Response.new
   
     if req.path.match(/item/)
-      binding.pry
-      @@items.each do |item|
-        resp.write "#{item.price}\n"
-      end
+      item_name = req.path.split("\/").last
+      
+      item = @@items.find {|i| i.name == item_name}
+      
+      resp.write "#{item.price}\n"
     else
       resp.write "Route not found"
       resp.status = 404
