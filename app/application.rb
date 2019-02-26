265f3d7e1cd39adb9babba47aa588a14eb1761e8
tgray017 <tgray017@gmail.com>
2019-02-26 14:40:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item_name = req.path.split("\/").last
       item = @@items.find {|i| i.name == item_name}
       if item.nil?
-        resp.write "Not found"
+        resp.write "Item not found"
         resp.status = 404
       else
         resp.write "#{item.price}\n"
